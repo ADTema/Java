3ae7426af6e4dc9c9ac3a8ca3eb7b9e0f3e75abe
ADTema <adavladvartem@yandex.ru>
2019-03-17 16:47:21 +0300

    Initial commit

---
+++ src/Bitset.java
@@ -0,0 +1,24 @@
+public class Bitset {
+    private int a;
+    private int b;
+    private int c;
+
+    public Bitset intersection(){
+
+    }
+    public Bitset association(){
+
+    }
+    public Bitset addition(){
+
+    }
+    public Bitset add(){
+
+    }
+    public Bitset remuve(){
+
+    }
+    public  Bitset accessory(){
+
+    }
+}
