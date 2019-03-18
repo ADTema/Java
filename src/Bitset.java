2ef104281994884e04fc529de5336075af8e239c
ADTema <adavladvartem@yandex.ru>
2019-03-18 10:51:44 +0300

    по максимуму все переделываю

--- src/Bitset.java
+++ src/Bitset.java
@@ -1,24 +1,50 @@
 public class Bitset {
-    private int a;
-    private int b;
-    private int c;
+    private int[] array1;
+    private int[] array2;
+    private int search;
 
-    public Bitset intersection(){
-
-    }
-    public Bitset association(){
+    //Пересечение
+    public Bitset intersection (int[] array1, int[] array2){
 
     }
-    public Bitset addition(){
+    //Объединение
+    public Bitset association (int[] array1, int[] array2){
 
     }
-    public Bitset add(){
+    //Дополнение
+    public Bitset addition (int[] array1, int[] array2){
 
     }
-    public Bitset remuve(){
+    //Добавление
+    public Bitset add (int[] array,int a ){
 
     }
-    public  Bitset accessory(){
-
+    //Удаление
+    public int[] remuve (int[] array1, int search,int[] array3){
+        search = 3;
+        for (int j = 0; j < array1.length; j++) { //поиск удаляемого элемента
+            for (int i = j; i < array1.length; i++) {
+                if (array1[i] == search) {
+                    for (int k = i; k < array1.length - 1; k++) { //сдвиг последующих элементов
+                        array1[k] = array1[k + 1];
+                        array3 = new int[array1.length-1];
+                        for(int g =0;g<array3.length;g++){
+                            array3[i]=array1[i];
+                        }
+                    }
+                }
+            }
+        }return array3;
+    }
+    //Принадлежность
+    public boolean accessory(int[] array1, int a, int[] array2){
+        boolean ans=false;
+        for(int i:array1) {
+            if(i==a) { //поиск элемента
+                ans=true;
+                break;
+            }
+        }
+        return ans;
     }
 }
--- src/Tests.java
+++ src/Tests.java
@@ -1,2 +1,9 @@
+
 public class Tests {
+    @org.junit.Test
+    public void accessory() {
+        Quaternion first = new Quaternion(10, -5, 0, 15);
+        Quaternion second = new Quaternion(30, -15, 0, 45);
+        Assert.assertTrue(first.accessory{1}.equalsRound(second, 1));
+    }
 }
