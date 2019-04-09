d88189591698c114d532e8a967082a6dc16a806a
ADTema <adavladvartem@yandex.ru>
2019-04-09 13:18:40 +0300

    Все готово

--- src/Bitset.java
+++ src/Bitset.java
@@ -1,52 +1,48 @@
-import java.util.Arrays;
-
 class Bitset{
     int[] array;//Массив
     private int size;
-
     Bitset(int size){
-        if(size%32==0) this.array=new int[size/32];
-        else this.array=new int[(size/32)+1];
-        for (int i=0;i<(size/32)-1;i++) this.array[i]=0;
+        if(size%Integer.SIZE==0) this.array=new int[size/Integer.SIZE];
+        else this.array=new int[(size/Integer.SIZE)+1];
+        for (int i=0;i<(size/Integer.SIZE)-1;i++) this.array[i]=0;
         this.size=size;
     }
 
     //Удаление
     public void remove(int number){//Меняет бит под определенным номером с 1 на 0
-        if(number<=this.size){
-            int num=number%32;
-            int r=Integer.MAX_VALUE-1;
-            if (number%32==0){
-                number=(number/32)-1;
-            }else {
-                number = (int)Math.floor(number >> 5);
-                for(int i=0;i<32-num;i++){
-                    r=r<<(1);
-                    r|=1;
-                }
+        if(number>this.size) throw new ArrayIndexOutOfBoundsException();
+
+        int num=number%Integer.SIZE;
+        int r=Integer.MAX_VALUE-1;
+        if (number%Integer.SIZE==0){
+            number=(number/Integer.SIZE)-1;
+        }else {
+            number = number / Integer.SIZE;
+            for(int i=0;i<Integer.SIZE-num;i++){
+                r=r<<(1);
+                r|=1;
             }
-            this.array[number]&=r;
-        }else throw new ArrayIndexOutOfBoundsException();
+        }
+        this.array[number]&=r;
     }
 
     //Добавление
     public void add(int number){//Меняет бит под определенным номером с 0 на 1
-        if(number<=this.size){
-            int num=number%32;
-            int r=1;
-            if (number%32==0){
-                number=(number/32)-1;
-            }else {
-            number = (int)Math.floor(number >> 5);
-            r=r<<(32-num);
-            }
-            this.array[number]|=r;
-        }else throw new ArrayIndexOutOfBoundsException();
+        if(number>this.size)throw new ArrayIndexOutOfBoundsException();
+        int num=number%Integer.SIZE;
+        int r=1;
+        if (number%Integer.SIZE==0){
+            number=(number/Integer.SIZE)-1;
+        }else {
+        number = number/(Integer.SIZE+1);
+        r=r<<(Integer.SIZE-num);
+        }
+        this.array[number]|=r;
     }
 
     //Пересечение
-    public Bitset intersection (Bitset array2){
-        Bitset inter=new Bitset(0);
+    public Bitset intersect (Bitset array2){
+        Bitset inter;
         if(this.size>array2.size){
             inter=new Bitset(array2.size);
         }else {
@@ -54,24 +50,22 @@ class Bitset{
         }
         for (int i=0;i<inter.array.length;i++){
             inter.array[i]=this.array[i]&array2.array[i];
-            System.out.println(Arrays.toString(inter.array));
-        }return inter;
+        }
+        return inter;
     }
 
     //Принадлежность
-    public boolean accessory(int number){
+    public boolean contain (int number){
         boolean ans=false;
-        int by=this.array[(int) Math.ceil(number/33)];
-        for(int i=0;i<(number%32);i++){
-            by/=2;
-        }
+        int by=this.array[number/(Integer.SIZE+1)];
+        by=by>>(Integer.SIZE-(number%Integer.SIZE));
         if (by%2==1) ans=true;
-        System.out.println(ans);
         return ans;
     }
+
     //Объединение
-    public Bitset association (Bitset array2){
-        Bitset inte=new Bitset(0);
+    public Bitset unite (Bitset array2){
+        Bitset inte;
         int in = 0;
         if(this.size>array2.size){
             inte=new Bitset(array2.size+(this.size-array2.size));
@@ -90,7 +84,16 @@ class Bitset{
                     inte.array[i]=this.array[i];
                 }
             }
-        }System.out.println(Arrays.toString(inte.array));
+        }
         return inte;
+    }
+
+    //Дополнение
+    public Bitset comp (){
+        Bitset array2 = new Bitset(this.size);
+        for(int i=0;i<this.size/Integer.SIZE;i++){
+            array2.array[i]=~this.array[i];
+        }
+        return array2;
     }
 }
--- src/BitsetTest.java
+++ src/BitsetTest.java
@@ -3,12 +3,9 @@ import java.util.Arrays;
 public class BitsetTest {
     @org.junit.Test
     public void Bitset() {
-        Bitset qwe=new Bitset(33);// 128 это количество бит, тут будет 4 числа по 32 бита
-        qwe.add(33); // 126 это номер бита который мы меняем
-        qwe.add(30);
-        Bitset qw=new Bitset(32);
-        qw.add(30);
-        qw.intersection(qwe);
+        Bitset qwe=new Bitset(320);// 128 это количество бит, тут будет 4 числа по 32 бита
+        qwe.add(320); // 126 это номер бита который мы меняем
+        qwe.contain(32);
         System.out.println(Arrays.toString(qwe.array));
     }
 }
